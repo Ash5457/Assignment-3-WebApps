ae990985851287d9b7edabcfa05d0feb5b41a4cf
Farzad I. Student <theeonsamurai@gmail.com>
2023-11-19 15:51:20 -0500

    adding the extra files and folders

---
+++ includes/nav.php
@@ -0,0 +1,9 @@
+ <nav>
+        <!-- The following nav will be a bar at the top of the webpage so users can visit the desired page-->
+        <ul>
+          <li><a href="./index.php"><i class="fa-solid fa-house"></i></a></li>
+          <li><a href="./list.php"><i class="fa-solid fa-clipboard-list"></i></a></li>
+          <li><a href="./login.php"><i class="fa-solid fa-right-to-bracket"></i></a></li>
+          <li><a href="./search.php"><i class="fa-solid fa-magnifying-glass"></i></a></li>
+        </ul>
+      </nav>
